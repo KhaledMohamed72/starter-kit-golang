3164339bb4fdd81f620d0e411f6ed1631b91598b
KhaledMohamed72 <khaledmohamed7298@gmail.com>
2022-05-31 12:06:37 +0200

    test2

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ func main() {
 	application := app()
 
 	application.Gin.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "khaledd",
+		request := newRequest(c)
+		request.Context.JSON(200, gin.H{
+			"message": "khaled",
 		})
 	})
 	application.Gin.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
+
+// We have two closure
+// Application closure --> hold global values
+// Request closure --> hold temp values when a user call an api
